544130e37131f297b3057983f03fd07604a0f7fe
serafim <serafim.urukov@gmail.com>
2017-09-15 12:31:25 +0300

    Отрисовал координаты. Сменил ракурс обзора. Сделал возможным управление камерой кнопками W S.

--- First.py
+++ First.py
@@ -10,9 +10,25 @@ vertices = (
         (2, 0, 0), (2, 0, 2), (0, 0, 2), (0, 0, 0), (2, 1, 2), (0, 1, 2), (2, 1, 1), (0, 1, 1), (2, 2, 1), (0, 2, 1),
         (0, 2, 0), (2, 2, 0))
 
-def keyPressed(*args):
-    if args[0] == ESCAPE:
-        sys.exit()
+eye = (2,3,5)
+
+def keyPressed(bkey, x, y):
+    global eye
+    # Convert bytes object to string
+    try:
+        key = bkey.decode("utf-8")
+        # Allow to quit by pressing 'Esc' or 'q'
+        if key == chr(27):
+            sys.exit()
+        if key == 'q':
+            print("Bye!")
+            sys.exit()
+        if key == 'w':
+            eye = (eye[0]+1, eye[1], eye[2])
+        if key == 's':
+            eye = (eye[0]-1, eye[1], eye[2])
+    except:
+        pass
 
 
 def initFigure():
@@ -21,14 +37,30 @@ def initFigure():
         (8, 11), (9, 10), (10, 3), (10, 11))
 
     glBegin(GL_LINES)
+    glColor3f(0, 0, 0);
     for edge in edges:
         for vertex in edge:
             glVertex3fv(vertices[vertex])
     glEnd()
 
+def init_axes():
+    glBegin(GL_LINES);
+    glColor3f(1, 0, 0);
+    glVertex3f(-100, 0, 0);
+    glVertex3f(100, 0, 0);
+
+    glColor3f(0, 1, 0);
+    glVertex3f(0, -100, 0);
+    glVertex3f(0, 100, 0);
+
+    glColor3f(0, 0, 1);
+    glVertex3f(0, 0, -100);
+    glVertex3f(0, 0, 100);
+
+    glEnd();
 
 def initGL(Width, Height):
-    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glClearColor(1.0, 1.0, 1.0, 1.0)
     glClearDepth(1.0)
     glDepthFunc(GL_LESS)
     glEnable(GL_DEPTH_TEST)
@@ -43,9 +75,13 @@ def drawGLScene():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
     glLoadIdentity()
-    glTranslatef(0.0, 0.0, -6.0)
 
+    gluLookAt(eye[0], eye[1], eye[2], 0, 0, 0, 0, 1, 0)
+    #glTranslatef(0.0, 0.0, -6.0)
+
+    init_axes()
     initFigure()
+
 
     glutSwapBuffers()
 
@@ -66,6 +102,6 @@ def main():
     initGL(640, 480)
     glutMainLoop()
 
-
 if __name__ == "__main__":
+    print("Hit ESC key to quit.")
     main()
